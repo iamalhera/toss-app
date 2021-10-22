14b2f7bbea4e31c624cbf0dfe081b0d64967e450
Al Hera Ahmad <alheraahmad0786@gmail.com>
2021-10-22 16:38:24 +0530

    complete styling done

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ var checkButton = document.querySelector("#check");
 var result = document.querySelector("#result");
 
 
-checkButton.addEventListener('click',()=>{
+checkButton.addEventListener('click', () => {
     var num = Math.random();
-    var binary = Math.round(num) ;
+    var binary = Math.round(num);
     // console.log(tossOptions.value);
     // console.log(binary);
-    if(Number(binary) === Number(tossOptions.value)){
-        result.innerText = "Congratulations! You won";
+    if (Number(tossOptions.value) === 0 || Number(tossOptions.value) === 1) {
+        console.log(tossOptions.value);
+        if (Number(binary) === Number(tossOptions.value)) {
+            result.innerText = "Congratulations! You won";
+            result.style.color = "Green" ;
+        }
+        else {
+            result.innerText = "Sorry! You lose";
+            result.style.color = "blue";
+        }
     }
     else{
-        result.innerText = "Sorry! You lose";
-    }
+        result.innerText = "Please Select the options !";
+        result.style.color = "red";
+        }
 })
